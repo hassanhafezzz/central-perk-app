052eb8a040868f20b486bdc285f4663a92281061
Hassan Hafez <hassan1hafez@gmail.com>
2020-01-01 03:05:57 +0200

    reduce js bundle size

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
-// import React, { Component, Suspense, lazy } from 'react';
-import React, { Component } from 'react';
+import React, { Component, Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './components/Home';
-import Products from './components/Products';
 import Nav from './components/common/Nav';
 import Footer from './components/common/Footer';
-
-/* ----------
-    -- For Better Performance --
-  == remove unnecessary packages ==
-----------  */
-/* eslint-disable */
-import Swiper from 'react-id-swiper';
-import rd3 from 'react-d3-library';
-import axios from 'axios';
-import { Formik } from 'formik';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import { OrderedMap } from 'immutable';
-import { Calendar, momentLocalizer } from 'react-big-calendar';
-import moment from 'moment';
-/* eslint-enable */
-
-/* ----------
- -- For Better Performance --
-  == use code splitting ==
-----------  */
-// import Loader from './components/common/Loader';
-// const Home = lazy(() => import('./components/Home'));
-// const Products = lazy(() => import('./components/Products'));
+import Loader from './components/common/Loader';
+const Home = lazy(() => import('./components/Home'));
+const Products = lazy(() => import('./components/Products'));
 
 class App extends Component {
   constructor(props) {
@@ -43,37 +19,19 @@ class App extends Component {
     return (
       <div className="app">
         <Router>
-          <Nav />
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route exact path="/products">
-              <Products />
-            </Route>
-          </Switch>
-          <Footer />
+          <Suspense fallback={<Loader />}>
+            <Nav />
+            <Switch>
+              <Route exact path="/products">
+                <Products />
+              </Route>
+              <Route exact path="/">
+                <Home />
+              </Route>
+            </Switch>
+            <Footer />
+          </Suspense>
         </Router>
-        {/* ----------
-          -- For Better Performance --
-            == use code splitting ==
-          ---------- /* }
-        {/*
-          <Router>
-            <Suspense fallback={<Loader />}>
-              <Nav />
-              <Switch>
-                <Route path="/products">
-                  <Products />
-                </Route>
-                <Route path="/">
-                  <Home />
-                </Route>
-              </Switch>
-              <Footer />
-            </Suspense>
-          </Router>
-        */}
       </div>
     );
   }

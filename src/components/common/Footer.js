4c17ae669d0e31cef2aa1e6b68818d97d40a6d60
Hassan Hafez <hassan1hafez@gmail.com>
2020-01-01 03:05:57 +0200

    reduce js bundle size

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,10 +1,4 @@
 import React from 'react';
-import moment from 'moment';
-/* ----------
-          -- For Better Performance --
-  == if you only need a simple functionality, ==
-    == no need to import a library for it ==
-----------  */
 
 export default function Footer() {
   return (
@@ -17,8 +11,7 @@ export default function Footer() {
       <a href="https://twitter.com/HassanHafezz" rel="noopener noreferrer" target="_blank">
         @hassanhafezz
       </a>{' '}
-      <span className="now">{moment().format('YYYY')}</span>
-      {/* <span className="now">{new Date().getFullYear()}</span> */}
+      <span className="now">{new Date().getFullYear()}</span>
     </footer>
   );
 }
